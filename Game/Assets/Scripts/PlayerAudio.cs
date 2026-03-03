c05b0c3390add7f57fded9a4e65ce2f4507300f6
Natalie Glavak <natalieglavak@MacBook-Air-som-tillhor-Natalie.local>
2026-03-03 16:02:06 +0100

    added Player Audio

---
+++ Game/Assets/Scripts/PlayerAudio.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class PlayerAudio : MonoBehaviour
+{
+    [SerializeField] private AudioSource sfxSource;
+    [SerializeField] private AudioClip crashClip;
+    [SerializeField] private AudioClip gameOverClip;
+
+    private bool hasDied = false;
+
+    private void Awake()
+    {
+        if (sfxSource == null)
+            sfxSource = GetComponent<AudioSource>();
+    }
+
+    public void DieWithSounds()
+    {
+        if (hasDied) return;
+        hasDied = true;
+
+        sfxSource.PlayOneShot(crashClip);
+        Invoke(nameof(PlayGameOver), 0.4f);
+        StartCoroutine(LoadSceneAfterDelay());
+    }
+
+    private void PlayGameOver()
+    {
+        sfxSource.PlayOneShot(gameOverClip);
+    }
+
+    private IEnumerator LoadSceneAfterDelay()
+    {
+        yield return new WaitForSeconds(1.2f);
+        SceneManager.LoadScene("MainViewScene");
+    }
+}
